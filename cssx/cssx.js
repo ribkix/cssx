db5a70fa2abf488f2ed965c37d42476709fac4d6
ribkix <42357978+ribkix@users.noreply.github.com>
2020-09-13 19:37:34 +0200

    Update cssx.js

--- cssx/cssx.js
+++ cssx/cssx.js
@@ -1,9 +1,9 @@
-$(function cssx_init_navbar(nav_class) {
+function cssx_init_navbar(nav_class) {
   $(document).scroll(function () {
     var $nav = $(nav_class);
     $nav.toggleClass("scrolled-nav", $(this).scrollTop() > $nav.height());
   });
-});
+}
 
 function cssx_body_theme_light(){
   document.body.classList.remove("theme-dark");
@@ -17,28 +17,28 @@ function cssx_body_theme_dark(){
 
 function cssx_body_theme_toggle(){
   if (document.body.classList.contains("theme-light")){
-    body_theme_dark(); 
+    cssx_body_theme_dark(); 
   } else {
-    body_theme_light();
+    cssx_body_theme_light();
   }
 }
 
 function cssx_body_theme_light_save(){
-  body_theme_light();
+  cssx_body_theme_light();
   localStorage.setItem("theme","light");
 }
 
 function cssx_body_theme_dark_save(){
-  body_theme_dark();
+  cssx_body_theme_dark();
   localStorage.setItem("theme","dark");
 }
 
 function cssx_body_theme_toggle_save(){
   if (document.body.classList.contains("theme-light")){
-    body_theme_dark();
+    cssx_body_theme_dark();
     localStorage.setItem("theme","dark");
   } else {
-    body_theme_light();
+    cssx_body_theme_light();
     localStorage.setItem("theme","light");
   }
 }
@@ -46,9 +46,9 @@ function cssx_body_theme_toggle_save(){
 function cssx_body_theme_load(){
   if (localStorage.getItem("theme")){
     if (localStorage.getItem("theme") == "dark"){
-      body_theme_dark();
+      cssx_body_theme_dark();
     } else {
-      body_theme_light();
+      cssx_body_theme_light();
     }
   }
 }
