090dc72fbd670c5e2dd3f5f005ef36cd51041f62
ribkix <42357978+ribkix@users.noreply.github.com>
2020-09-13 18:14:27 +0200

    Update cssx.js

--- cssx/cssx.js
+++ cssx/cssx.js
@@ -1,6 +1,24 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar");
-    $nav.toggleClass('scrolled-nav', $(this).scrollTop() > $nav.height());
+    $nav.toggleClass("scrolled-nav", $(this).scrollTop() > $nav.height());
   });
 });
+
+function body_theme_light(){
+  document.body.classList.remove("theme-dark");
+  document.body.classList.add("theme-light");
+}
+
+function body_theme_dark(){
+  document.body.classList.remove("theme-light");
+  document.body.classList.add("theme-dark");
+}
+
+function body_theme_toggle(){
+  if (document.body.classList.contains("theme-light")){
+    body_theme_dark(); 
+  } else {
+    body_theme_light();
+  }
+}
